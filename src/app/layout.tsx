129be7dd3ee2996d4e915823692ebf3da70209c9
pedro <pedro.adoneto@gmail.com>
2024-10-22 17:12:27 -0400

    added web prview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,22 @@
+export const metadata = {
+  title: 'Marcella Cavalcanti - Portfolio',
+  description: 'I am an UX/UI Designer who values efficiency, uniqueness, and creativity, with an approach that combines psychology and design to create impactful and functional user experiences.',
+  openGraph: {
+    title: 'Marcella Cavalcanti - Portfolio',
+    description: 'I am an UX/UI Designer who values efficiency, uniqueness, and creativity, with an approach that combines psychology and design to create impactful and functional user experiences.',
+    images: [
+      {
+        url: '/images/image_preview.png',
+        width: 1800,
+        height: 840,
+        alt: 'Marcella Cavalcanti Design',
+      },
+    ],
+    url: 'https://marcella-portfolio.vercel.app/',
+    type: 'website',
+  }
+};
+
 import HeaderContainer from "@/containers/header/HeaderContainer";
 import "../styles/globals.css";
 
