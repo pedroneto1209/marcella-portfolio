5924ed4c4fbe4f871cb87300cdaca01fe6ffeb9b
pedro <pedro.adoneto@gmail.com>
2024-10-22 17:17:30 -0400

    fixed web prev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 export const metadata = {
   title: 'Marcella Cavalcanti - Portfolio',
-  description: 'I am an UX/UI Designer who values efficiency, uniqueness, and creativity, with an approach that combines psychology and design to create impactful and functional user experiences.',
+  description: 'I am a UX/UI Designer who values efficiency, uniqueness, and creativity, with an approach that combines psychology and design to create impactful and functional user experiences.',
   openGraph: {
     title: 'Marcella Cavalcanti - Portfolio',
-    description: 'I am an UX/UI Designer who values efficiency, uniqueness, and creativity, with an approach that combines psychology and design to create impactful and functional user experiences.',
+    description: 'I am a UX/UI Designer who values efficiency, uniqueness, and creativity, with an approach that combines psychology and design to create impactful and functional user experiences.',
     images: [
       {
-        url: '/images/image_preview.png',
+        url: '/images/image_preview.png', // Relative path to the image in the `public` folder
         width: 1800,
         height: 840,
         alt: 'Marcella Cavalcanti Design',
@@ -14,8 +14,15 @@ export const metadata = {
     ],
     url: 'https://marcella-portfolio.vercel.app/',
     type: 'website',
-  }
+  },
+  twitter: {
+    card: 'summary_large_image', // Use 'summary_large_image' to ensure a large preview
+    title: 'Marcella Cavalcanti - Portfolio',
+    description: 'I am a UX/UI Designer who values efficiency, uniqueness, and creativity, with an approach that combines psychology and design to create impactful and functional user experiences.',
+    images: ['/images/image_preview.png'], // Ensure the image path is consistent
+  },
 };
+
 
 import HeaderContainer from "@/containers/header/HeaderContainer";
 import "../styles/globals.css";
